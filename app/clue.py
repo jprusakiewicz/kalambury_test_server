2a0bae0ec68d6091d9ebd365e2f2e4c3f0a17359
Kuba Prusakiewicz <jakubprusakiewicz@gmail.com>
2021-11-29 20:22:22 +0100

    draw of the same category blocked

--- app/clue.py
+++ app/clue.py
@@ -8,6 +8,7 @@ class ClueManager:
     def __init__(self, locale):
         self.clue_dict = self.read_clue(locale)
         self.used_clues = []
+        self.last_category = None
 
     def read_clue(self, locale):
         if locale in ['pl', 'en', 'es', 'de', 'fr', 'it']:
@@ -20,9 +21,13 @@ class ClueManager:
     def get_new_clue(self) -> (str, str):
         category = random.choice(list(self.clue_dict.keys()))
         clue = random.choice(self.clue_dict[category])
-        if clue in self.used_clues:
+        if clue in self.used_clues or category == self.last_category:
             category, clue = self.get_new_clue()
-            if len(self.used_clues) > 99:
-                self.used_clues = self.used_clues[:-5]
+
+        if len(self.used_clues) > 95:
+            self.used_clues = self.used_clues[-5:]
+
         self.used_clues.append(clue)
+        self.last_category = category
+
         return category, clue
